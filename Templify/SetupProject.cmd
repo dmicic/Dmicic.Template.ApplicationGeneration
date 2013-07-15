4af786069f3f1c54d6753734e3c312a3d1bf5403
dmicic <darko@micic.ch>
2013-07-15 10:57:53 +0200

    set command

--- Templify/SetupProject.cmd
+++ Templify/SetupProject.cmd
@@ -1,9 +1,7 @@
-setlocal EnableDelayedExpansion  
+set TemplateName=application-generation-mvc-v1.0
 
-setlocal TemplateName=application-generation-mvc-v1.0
-
-setlocal DestinationFolder="C:\..."
-setlocal TemplateFolder="C:\..."
-setlocal SolutionName=MyNewSolution
+set DestinationFolder="C:\..."
+set TemplateFolder="C:\..."
+set SolutionName=MyNewSolution
 
 Templify\TemplifyCmd.exe -m d -p %DestinationFolder% -i %TemplateName% -t __NAME__=%SolutionName% -r %TemplateFolder%
